f1444232a9ef9d9d1e24c3daeab5b653b096461c
assiafa <145739532+assiafa@users.noreply.github.com>
2023-11-13 10:10:28 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function changeContentBackgroundColor(element) {
+    element.style.backgroundColor = 'gray';
+    
+
+  }
+
+function changeContent(textElement){
+    textElement.style.fontSize = '50px';
+    textElement.style.color = '#09796fe1';
+}
+
+function resetContent(textElement){
+    textElement.style.fontSize = '30px';
+    textElement.style.color = 'brown';
+}
+  function resetContentBackgroundColor(element) {
+    element.style.backgroundColor = '#09796fe1';
+  }
